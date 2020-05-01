1cfc824c333a5d317b9629dcd3e3e721131fa407
singular-steve <singular1.steve@gmail.com>
2020-05-01 23:30:25 +0900

    add sample code for https server

---
+++ ch4.web-server/4.4.https.js
@@ -0,0 +1,16 @@
+const https = require('https');
+const fs = require('fs');
+
+https.creatServer({
+  cert: fs.readFileSync('도메인 인증서 경로'),
+  key: fs.readFileSync('도메인 비밀키 경로'),
+  ca: [
+    fs.readFileSync('상위 인증서 경로'),
+    fs.readFileSync('상위 인증서 경로'),
+  ],
+}, (req, res) => {
+  res.write('<h1>Hello World!</h1>');
+  res.end('<h1>Hello World!</h1>');
+}).listen(443, () => {
+  console.log('listening on 443');
+});
