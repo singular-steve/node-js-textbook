67ca5cba552c7857ba37cbb4f0d2ab6f2a64d3dc
raw-coder <singular1.steve@gmail.com>
2020-05-11 09:03:50 +0900

    add sample code for comment app by using mongoose

--- ch8.mongodb/public/mongoose.js
+++ ch8.mongodb/public/mongoose.js
@@ -41,3 +41,131 @@ function getUser() {
   xhr.open('GET', '/users');
   xhr.send();
 }
+
+function getComment(id) {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const comments = JSON.parse(xhr.responseText);
+      const tbody = document.querySelector('#comment-lst tbody');
+      tbody.innerHTML = '';
+      comments.map(comment => {
+        let row = document.createElement('tr');
+        let td = document.createElement('td');
+        td.textContent = comment._id;
+        row.appendChild(td);
+        td = document.createElement('td');
+        td.textContent = comment.commenter.name;
+        row.appendChild(td);
+        td = document.createElement('td');
+        td.textContent = comment.comment;
+        row.appendChild(td);
+        td = document.createElement('td');
+        
+        const edit = document.createElement('button');
+        edit.textContent = 'EDIT';
+        edit.addEventListener('click', () => {
+          const newComment = prompt('Input your comment!');
+          if (!newComment) {
+            return alert('Input your comment here!!!');
+          }
+          const xhr = new XMLHttpRequest();
+          xhr.onload = () => {
+            if (xhr.status === 200) {
+              console.log(xhr.responseText);
+              getComment(id);
+            } else {
+              console.error(xhr.responseText);
+            }
+          };
+          xhr.open('PATCH', '/comments/' + comment._id);
+          xhr.setRequestHeader('Content-Type', 'application/json');
+          xhr.send(JSON.stringify({ comment: newComment }));
+        });
+  
+        const remove = document.createElement('button');
+        remove.textContent = 'REMOVE';
+        remove.addEventListener('click', () => {
+          const xhr = new XMLHttpRequest();
+          xhr.onload = () => {
+            if (xhr.status === 200) {
+              console.log(xhr.responseText);
+              getComment(id);
+            } else {
+              console.error(xhr.responseText);
+            }
+          };
+          xhr.open('DELETE', '/comments/' + comment._id);
+          xhr.send(JSON.stringify({ comment: newComment }));
+        });
+        
+        td = document.createElement('td');
+        td.appendChild(edit);
+        row.appendChild(td);
+        td = document.createElement('td');
+        td.appendChild(remove);
+        row.appendChild(td);
+        
+        tbody.appendChild(row);
+      });
+    } else {
+      console.error(xhr.responseText);
+    }
+  };
+  xhr.open('GET', '/comments/' + id);
+  xhr.send();
+}
+
+document.getElementById('user-form').addEventListener('submit', ev => {
+  ev.preventDefault();
+  const name = ev.target.username.value;
+  const age = ev.target.age.value;
+  const married = ev.target.married.checked;
+  if (!name) {
+    return alert('Input your name!');
+  }
+  if (!age) {
+    return alert('Input your age!');
+  }
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    if (xhr.status === 201) {
+      console.log(xhr.responseText);
+      getUser();
+    } else {
+      console.error(xhr.responseText);
+    }
+  };
+  xhr.open('POST', '/users');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({ name, age, married }));
+  ev.target.username.value = '';
+  ev.target.age.value = '';
+  ev.target.married.checked = false;
+});
+
+document.getElementById('comment-form').addEventListener('submit', ev => {
+  ev.preventDefault();
+  const id = ev.target.userId.value;
+  const comment = ev.target.comment.value;
+  if (!id) {
+    return alert('Input your Id!');
+  }
+  if (!comment) {
+    return alert('Input your comment!');
+  }
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    if (xhr.status === 201) {
+      console.log(xhr.responseText);
+      getComment(id);
+    } else {
+      console.error(xhr.responseText);
+    }
+  };
+  xhr.open('POST', '/comments');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({ id, comment }));
+  ev.target.id.value = '';
+  ev.target.comment.value = '';
+});
