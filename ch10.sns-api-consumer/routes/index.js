a9aff5d0451abf984306520025ae0f5ae765169f
raw-coder <singular1.steve@gmail.com>
2020-05-19 21:05:06 +0900

    add GET /posts/my add GET /posts/hashtag/:title

--- ch10.sns-api-consumer/routes/index.js
+++ ch10.sns-api-consumer/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const URL = 'http://localhost:8002/v1';
 
 router.get('/test', async (req, res, next) => {
   try {
@@ -24,6 +25,52 @@ router.get('/test', async (req, res, next) => {
       return res.json(error.response.data);
     }
     return next(error)
+  }
+});
+
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) {
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token;
+    }
+    return await axios.get(`${URL}${api}`, {
+      headers: {
+        authorization: req.session.jwt,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    if (error.response.status < 500) {
+      return error.response;
+    }
+    throw error;
+  }
+}
+
+router.get('/mypost', async (req, res, next) => {
+  try {
+    const result = await request(req, '/posts/my');
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+  try {
+    const result = await request(
+      req, `/post/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+    );
+    res.json(result.data);
+  } catch (error) {
+    if (error.code) {
+      console.error(error);
+      next(error);
+    }
   }
 });
 
--- ch10.sns-api-provider/routes/v1.js
+++ ch10.sns-api-provider/routes/v1.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken } = require('./middlewares');
-const { Domain, User, Post, Hashtag } = require('../models');
+const {verifyToken} = require('./middlewares');
+const {Domain, User, Post, Hashtag} = require('../models');
 
 const router = express.Router();
 
 router.post('/token', async (req, res) => {
-  const { clientSecret } = req.body;
+  const {clientSecret} = req.body;
   try {
     const domain = await Domain.findOne({
-      where: { clientSecret },
+      where: {clientSecret},
       include: {
         model: User,
         attribute: ['nick', 'id'],
@@ -45,6 +45,53 @@ router.post('/token', async (req, res) => {
 
 router.get('/test', verifyToken, (req, res) => {
   res.json(req.decoded);
+});
+
+router.get('/posts/my', verifyToken, (req, res) => {
+  Post.findAll({
+    where: {userId: req.decoded.id},
+  })
+    .then(posts => {
+      console.log(posts);
+      res.json({
+        code: 200,
+        payload: posts,
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      return res.status(500).json({
+        code: 500,
+        message: 'Internal Server Error',
+      });
+    });
+});
+
+router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
+  try {
+    const hashtag = await Hashtag.findOne({
+      where: {
+        title: req.params.title,
+      },
+    });
+    if (!hashtag) {
+      return res.status(404).json({
+        code: 404,
+        message: 'No Hashtags',
+      });
+    }
+    const posts = await hashtag.getPosts();
+    return res.json({
+      code: 200,
+      payloads: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: 'Server Error',
+    });
+  }
 });
 
 module.exports = router;
