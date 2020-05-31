7548bd56b4f34cf5bcb3f42228bfc3b598711673
raw-coder <singular1.steve@gmail.com>
2020-05-31 23:08:05 +0900

    add production env setting to sns app

--- ch9.sns/app.js
+++ ch9.sns/app.js
@@ -23,7 +23,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8001);
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
@@ -31,7 +35,8 @@ app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+
+const sessionOption = {
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -39,7 +44,15 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+};
+
+if (process.env.NODE_ENV === 'production') {
+  sessionOption.proxy = true;
+  sessionOption.cookie.secure = true;
+}
+
+app.use(session(sessionOption));
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
